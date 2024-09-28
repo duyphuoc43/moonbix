70ba7ccac65cf3e2adbd978fcca77db75a7de242
Duy Phước <135586447+duyphuoc43@users.noreply.github.com>
2024-09-28 23:02:12 +0700

    Update detection.py

--- detection.py
+++ detection.py
@@ -33,15 +33,12 @@ while True:
     # Sử dụng mô hình YOLO để phát hiện đối tượng
     results = model(img, verbose=False)
         # Hiển thị kết quả với hộp giới hạn
-    annotated_frame = results[0].plot()  # Vẽ các hộp bounding box lên frame
+    # annotated_frame = results[0].plot()  # Vẽ các hộp bounding box lên frame
     
-    # Hiển thị frame
-    cv2.imshow("Screen Capture with YOLOv8 Detection", annotated_frame)
+    # # Hiển thị frame
+    # cv2.imshow("Screen Capture with YOLOv8 Detection", annotated_frame)
     for result in results:
-        print(len(results))
         detections = [[],[],[],[]]
-
-        
         if result.boxes is not None:
             for box in result.boxes:
                 x_min, y_min, x_max, y_max = [coord.item() for coord in box.xyxy[0]]
@@ -61,43 +58,24 @@ while True:
 
         boom = detections[0]
         gold = detections[1]
-        if(len(detections[2]) > 0):
+
+        if(len(detections[2]) > 0 and len(detections[3]) > 0):
             moc = detections[2]
-        else:
-            moc = [{'x_box': 266.13214111328125,
-            'y_box': 204.82144165039062,
-            'w_box': 25.680084228515625,
-            'h_box': 20.217391967773438,
-            'center_x': 278.97218322753906,
-            'center_y': 214.93013763427734,
-            'confidence': 0.74,
-            'class_id': 2}]
-        if(len(detections[3]) > 0):
             phithuyen = detections[3]
-        else:
-            phithuyen = [{'x_box': 221.18624877929688,
-                    'y_box': 144.13951110839844,
-                    'w_box': 67.80099487304688,
-                    'h_box': 55.251617431640625,
-                    'center_x': 255.0867462158203,
-                    'center_y': 171.76531982421875,
-                    'confidence': 0.78,
-                    'class_id': 3}]
 
-        x1 = phithuyen[0]["center_x"]
-        y1 = phithuyen[0]["center_x"]
+            x1 = phithuyen[0]["center_x"]
+            y1 = phithuyen[0]["center_x"]
 
-        x2 = moc[0]["center_x"]
-        y2 = moc[0]["center_x"]
+            x2 = moc[0]["center_x"]
+            y2 = moc[0]["center_x"]
 
-        for item in gold:
-            x3 = item["center_x"]
-            y3 = item["center_x"]
+            for item in gold:
+                x3 = item["center_x"]
+                y3 = item["center_x"]
 
-            if((y2 - y1)*(x3 - x1) - (y3 - y1)*(x2 - x1) == 0):
-                # print("true")
-                pyautogui.click(150, 350)
-
+                if((y2 - y1)*(x3 - x1) - (y3 - y1)*(x2 - x1) == 0):
+                    pyautogui.click(150, 350)
+                    time.sleep(2) 
 
 
     # Thoát nếu nhấn phím 'q'
